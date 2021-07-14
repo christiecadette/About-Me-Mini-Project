dc6845fc088196980f18fc8182e9e660a9855a68
Christie Cadette <christiecadette@gmail.com>
2021-07-14 14:31:02 -0400

    Initial Commit

---
+++ About Me Mini Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  About Me Mini Project
+//
+//  Created by  Scholar on 7/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
